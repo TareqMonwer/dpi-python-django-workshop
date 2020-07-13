597f65c96c0aa8d05b8ede30f15d8a2e263a4cd7
tareqmonwer <tareqmonwer137@gmail.com>
2020-07-13 15:57:22 +0600

    Plan model basic version

--- readingtracker/models.py
+++ readingtracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 
+class Plan(models.Model):
+    LABELS = (
+        ('t', 'Top'),
+        ('m', 'Medium'),
+        ('l', 'Low')
+    )
+
+    plan_name = models.CharField(max_length=255)
+    time_starts = models.DateField()
+    time_ends = models.DateField()
+    label = models.CharField(choices=LABELS, 
+                             default='m',
+                             max_length=1)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.plan_name
